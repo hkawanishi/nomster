a01ee208fd25ff25f930c140e3d1fc6d6fb4b65d
Hiromi Kawanishi <contacthiromi@gmail.com>
2015-11-18 02:34:59 +0000

    add test for comment rating

--- app/assets/javascripts/places.js.coffee
+++ app/assets/javascripts/places.js.coffee
@@ -1,17 +1,16 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready ->
+#$(document).ready ->
  #$("a.fancybox").fancybox({
- $("a[rel=example_group]").fancybox({
-    #maxWidth : 1000,
-    #maxHeight : 1500,
-    maxWidth : 300,
-    maxHeight: 300;
-    scrolling : no,
-    #fitToView : true,
-    fitToView : false,
-    autoSize : true,
-    closeClick : true
-})
+#    #maxWidth : 1000,
+#    #maxHeight : 1500,
+#    maxWidth : 300,
+#    maxHeight: 300;
+##    scrolling : no,
+#    #fitToView : true,
+#    fitToView : false,
+#    autoSize : true,
+#    closeClick : true
+#})
 
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+ 
+  test "rating_check" do
+  	rating1 = Comment.new(:rating => '1_star').humanized_rating 
+  	expected = "one star"
+  	actual = rating1
+  	assert_equal expected, actual
+
+  	rating2 = Comment.new(:rating => '2_stars').humanized_rating 
+  	expected = "two stars"
+  	actual = rating2
+  	assert_equal expected, actual
+
+  	rating3 = Comment.new(:rating => '3_stars').humanized_rating 
+  	expected = "three stars"
+  	actual = rating3
+  	assert_equal expected, actual
+
+  	rating4 = Comment.new(:rating => '4_stars').humanized_rating 
+  	expected = "four stars"
+  	actual = rating4
+  	assert_equal expected, actual
+
+  	rating5 = Comment.new(:rating => '5_stars').humanized_rating 
+  	expected = "five stars"
+  	actual = rating5
+  	assert_equal expected, actual
+  end
 end
